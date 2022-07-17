0154b6cd597da47d002bf7aab1fcea307c2e7148
Le Ngoai Ngu <ngunl@capichiapp.com>
2022-07-17 08:59:16 +0700

    Bài 5: Tupple type

--- app.ts
+++ app.ts
@@ -1,23 +1,15 @@
-/** array type  */
+/** tupple type  */
 
 interface Person {
   name: string
   age: number
   hobbies: string[]
+  role: [number, string] // tupple
 }
 
 const phuong: Person = {
   name: 'Le Thi Phuong',
   age: 22,
-  hobbies: ['Sports', 'Cooking', 'Play game']
+  hobbies: ['Sports', 'Cooking', 'Play game'],
+  role: [1, 'Author']
 }
-
-for (const hobby of phuong.hobbies) {
-  console.log(hobby)
-}
-
-// -------------------
-
-let number: number[]
-
-number = [12, 23, 2, 34, 343]
