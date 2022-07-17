d036a409a70a1d38c2437f31933815c664fb3c52
Le Ngoai Ngu <ngunl@capichiapp.com>
2022-07-17 14:10:05 +0700

    Bài 12: Function Types and callback

--- app.ts
+++ app.ts
@@ -1,26 +1,11 @@
-/**  Function Type   */
+/**  Function Type and callback  */
 
-function add(n1: number, n2: number) {
-  return n1 + n2
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2
+
+  cb(result)
 }
 
-function printResult(num: number): void {
-  console.log('Result: ' + num)
-}
-
-// Function type
-
-let combineValue1: Function
-
-combineValue1 = printResult
-combineValue1 = add
-
-//combineValue1(1, 2) // Như này sẽ lỗi
-
-// Định nghĩa type cụ thể
-
-let combineValues: (a: number, b: number) => number
-
-combineValues = add
-
-//combineValues = printResult //type '(num: number) => void' is not assignable to type '(a: number, b: number) => number'. Type 'void' is not assignable to type 'number'.ts(2322)
+addAndHandle(1, 2, (result) => {
+  console.log(result)
+})
